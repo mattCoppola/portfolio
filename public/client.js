ba606b0c85b9e726b511a3cde751a96eea4ea0e8
Matt Coppola <matt.coppola@gmail.com>
2019-02-06 20:17:52 -0600

    more styling, added sections for placeholders, moved images folder into css folder

--- public/client.js
+++ public/client.js
@@ -1,6 +1,17 @@
 
 
 ///////////////////////////////////////////
+//on.click Button Scroll to Sections     //
+///////////////////////////////////////////
+
+$('.scroll').on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 500, 'linear');
+});
+
+///////////////////////////////////////////
 //on.click Hamburger Icon                //
 ///////////////////////////////////////////
 
