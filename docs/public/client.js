21020e613444ef74b6a1ce0f35daa4bed3996da4
Matt Coppola <matt.coppola@gmail.com>
2019-02-10 08:53:14 -0600

    added docs folder for github pages

---
+++ docs/public/client.js
@@ -0,0 +1,21 @@
+
+
+///////////////////////////////////////////
+//on.click Button Scroll to Sections     //
+///////////////////////////////////////////
+
+$('.scroll').on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top - 60
+    }, 500, 'linear');
+});
+
+///////////////////////////////////////////
+//on.click Hamburger Icon                //
+///////////////////////////////////////////
+
+// $('.fa-bars').on('click', function (e) {
+//     e.preventDefault();
+//     $('.main-nav li').toggleClass('responsive');
+// });
